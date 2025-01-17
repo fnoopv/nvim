a3a12cafba9467f9721900180fc231e42409d6d1
fnoopv <fnoopv@outlook.com>
2025-01-17 10:26:17 +0800

    update

---
+++ lua/plugins/plugin.lua
@@ -0,0 +1,46 @@
+return {
+  {
+    "williamboman/mason.nvim",
+    opts = { ensure_installed = { "impl", "gomodifytags", "golangci-lint" } },
+  },
+  {
+    "mikavilpas/yazi.nvim",
+    event = "VeryLazy",
+    keys = {
+      -- 👇 in this section, choose your own keymappings!
+      {
+        "<leader>yf",
+        "<cmd>Yazi<cr>",
+        desc = "Open yazi at the current file",
+      },
+      {
+        -- Open in the current working directory
+        "<leader>yw",
+        "<cmd>Yazi cwd<cr>",
+        desc = "Open the file manager in nvim's working directory",
+      },
+      {
+        -- NOTE: this requires a version of yazi that includes
+        -- https://github.com/sxyazi/yazi/pull/1305 from 2024-07-18
+        "<leader>yz",
+        "<cmd>Yazi toggle<cr>",
+        desc = "Resume the last yazi session",
+      },
+    },
+    opts = {
+      -- if you want to open yazi instead of netrw, see below for more info
+      open_for_directories = false,
+      keymaps = {
+        show_help = "?",
+      },
+    },
+  },
+  -- lazy.nvim
+  {
+    "folke/snacks.nvim",
+    ---@type snacks.Config
+    opts = {
+      lazygit = {},
+    },
+  },
+}
